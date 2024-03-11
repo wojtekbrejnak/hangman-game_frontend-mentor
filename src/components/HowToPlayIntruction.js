3f8f243a1ac5f399a8b9921a4296d3a3eb0e89c9
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-03-11 21:05:35 +0100

    Move section style to styled components

--- src/components/HowToPlayIntruction.js
+++ src/components/HowToPlayIntruction.js
@@ -1,10 +1,20 @@
+import styled from "styled-components";
+
+const StyledSection = styled.section`
+  background-color: white;
+  border-radius: 8.5%;
+  width: 40%;
+  padding: 5% 3.5%;
+  text-align: center;
+`;
+
 const HowToPlayInstruction = (props) => {
   return (
-    <section>
+    <StyledSection as="section">
       <h2>{props.number}</h2>
       <h3>{props.topic}</h3>
       <p>{props.paragraph}</p>
-    </section>
+    </StyledSection>
   );
 };
 
