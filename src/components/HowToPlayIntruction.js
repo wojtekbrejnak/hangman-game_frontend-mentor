f32ff80f6edc688871daca1c1a8ddf840fbb592a
wojtekbrejnak <150580013+wojtekbrejnak@users.noreply.github.com>
2024-03-11 21:08:51 +0100

    Move rest of section's style to styled components

--- src/components/HowToPlayIntruction.js
+++ src/components/HowToPlayIntruction.js
@@ -6,6 +6,33 @@ const StyledSection = styled.section`
   width: 40%;
   padding: 5% 3.5%;
   text-align: center;
+
+  h2,
+  h3,
+  p {
+    font-weight: lighter;
+  }
+
+  h2 {
+    font-size: var(--desktop-heading-l);
+    color: var(--clr-blue);
+    margin-bottom: 14.7%;
+  }
+
+  h3 {
+    font-size: var(--desktop-heading-m);
+    color: var(--clr-dark-navy);
+    letter-spacing: 0.05em;
+    margin-bottom: 15%;
+  }
+
+  p {
+    font-size: var(--desktop-body);
+    color: var(--clr-purple);
+    letter-spacing: 0.05em;
+    padding-inline: 0.2em;
+    line-height: 1.2em;
+  }
 `;
 
 const HowToPlayInstruction = (props) => {
